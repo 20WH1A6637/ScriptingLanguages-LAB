90a5c699c6a19b93d41b985394b2e73a58987b6b
Thopuri Harshitha <93579922+20WH1A6637@users.noreply.github.com>
2023-02-23 12:01:05 +0530

    Update exp7.rb

--- exp7.rb
+++ exp7.rb
@@ -1,4 +1,5 @@
-def makes20(x,y)    
-  return x == 20 || y == 20 || x + y == 20
+ef makes20(x,y)    
+  return x == 20 || y == 20 || x + y
 end
-print makes20(20, 20)
+print makes20(20, 20), "\n"
+print makes20(10, 15), "\n"
